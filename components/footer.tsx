50b09c3b6b0f54110fb9c93c668d79b7a19442ab
soheil <soheilrasekh555@gmail.com>
2022-06-05 16:09:33 +0430

    update readme

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,13 @@
-import { Container, Text, Box } from "@chakra-ui/react";
+import { Text, Box, Link } from "@chakra-ui/react";
 
 export default function Footer() {
   return (
     <Box textAlign="center">
       <Text color="gray.400" p={4}>
-        {new Date().getFullYear()} Soheil.
+        {new Date().getFullYear()} Soheil. Based on{" "}
+        <Link color="teal.500" isExternal href="https://www.craftz.dog/">
+          craftz.dog
+        </Link>
       </Text>
     </Box>
   );
